5fa0b9ed2e5020c00a9b965fd7e39fa723a03bd5
Anton Ashurek <83346452+AntonOshurek@users.noreply.github.com>
2021-06-09 23:23:47 +0200

    добавление js и правка html css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 //alert ('hello');
+
+let tab = function() {
+    let tabNav = document.querySelectorAll('.tabs-nav__item')
+    let tabContent = document.querySelectorAll('.tab')
+    let tabName;
+    //console.log(tabNav);
+    //console.log(tabContent);
+
+    tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+    });
+
+    function selectTabNav() {
+        tabName = this.getAttribute('data-tab-name');
+        selectTabContent(tabName);
+        console.log(tabName);
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+        })
+    }
+};
+
+tab();
