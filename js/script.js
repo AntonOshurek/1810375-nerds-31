aee0116d7ad8e9f5eaeffc0b447862cc27c690fd
Anton Ashurek <83346452+AntonOshurek@users.noreply.github.com>
2021-06-16 16:29:15 +0200

    adjusting styles and js

--- js/script.js
+++ js/script.js
@@ -31,14 +31,27 @@ const popup = document.getElementById('modal');  /*popup block*/
 const popupClose = document.getElementById('modal__close'); /*popup close btn*/
 const popupOpen = document.getElementById('modal__open'); /*popup open button*/
 const modalBg = document.getElementById('modal__bg');
+const submitBtn = document.querySelector('.modal__btn__submit'); //submit modal form btn
+
 
 const closeModal = () => {
     popup.classList.remove('modal__show');
-}
+};
 
 const openModal = () => {
     popup.classList.add('modal__show');
-}
+    modalKeyOpt();
+};
+
+function modalKeyOpt () {
+    window.onkeydown = ( event ) => {
+        if ( event.keyCode == 27 ) {
+            closeModal();
+        } else if (event.keyCode == 13) {
+            submitBtn.click();
+        }
+    };
+};
 
 popupOpen.addEventListener ('click', openModal);
 popupClose.addEventListener('click', closeModal);
